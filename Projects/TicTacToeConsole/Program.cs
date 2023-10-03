9a8a83ab9e34093974f3f61b7fa705efe9bceb83
seDrakonkill <samuel@kjcpa.com>
2023-10-03 12:43:38 -0500

    committed the starting code for the Tic Tac Toe
    Will be fleshing out later. Will merge with master once complete

---
+++ Projects/TicTacToeConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeConsole
+{
+    internal class TicTacToeInConsole
+    {
+        string[] board = { "0,0", "1,0", "2,0", "0,1", "0,2", "1,1", "1,2", "2,1", "2,2", };
+        static void Main()
+        {
+
+        }
+
+        static string PlayerXInput()
+        {
+            string playerInput = Console.ReadLine().ToString();
+            return playerInput;
+        }
+
+        static string PlayerOInput()
+        {
+            string playerInput = Console.ReadLine().ToString();
+            if (playerInput == "o" || playerInput == "O")
+            {
+                return playerInput;
+            }
+            else
+            {
+                Console.WriteLine("That is not the correct character. Please Enter O");
+                
+
+            }
+
+        }
+    }
+}
