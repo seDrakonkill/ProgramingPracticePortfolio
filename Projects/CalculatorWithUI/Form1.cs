f1ea7ae1d8daa6c43ff9d7ddcc15d542092a2c01
seDrakonkill <samuel@kjcpa.com>
2023-10-13 19:31:15 -0500

    Got some basic math functions whipped up.
    Moving onto the UI display.
    
    I didnt htink it would just be drag and drop for the layout creation. Got too big for my britches and tryied to type in the code are , stuff wnet funky. Ive learned my lsesson

--- Projects/CalculatorWithUI/Form1.cs
+++ Projects/CalculatorWithUI/Form1.cs
@@ -6,5 +6,25 @@ namespace CalculatorWithUI
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Projects/CalculatorWithUI/Program.cs
+++ Projects/CalculatorWithUI/Program.cs
@@ -1,6 +1,6 @@
 namespace CalculatorWithUI
 {
-    internal static class Program
+    internal static class UICalculator
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -10,8 +10,44 @@ namespace CalculatorWithUI
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            ApplicationConfiguration.Initialize(); 
+            Application.Run(new Form1());  //What does this do? It seems to call Form1.cs, which seems to initialize itself.
         }
+    }
+
+    internal static class Functions
+    {
+        static float storedVariable = 0;
+        public static float Add(float var1, float var2)
+        {
+            storedVariable = var1 + var2;
+            return 0;
+        }
+        public static float Subtract(float var1, float var2)
+        {
+            storedVariable = var1 + var2;
+            return 0;
+        }
+        public static float Multiply(float var1, float var2)
+        {
+            storedVariable = var1 + var2;
+            return 0;
+        }
+        public static float Divide(float var1, float var2)
+        {
+            storedVariable = var1 + var2;
+            return 0;
+        }
+        public static float SqRoot(float var1, float var2)
+        {
+            storedVariable = var1 + var2;
+            return 0;
+        }
+        public static float Log(float var1, float var2)
+        {
+            storedVariable = var1 + var2;
+            return 0;
+        }
+
     }
 }
