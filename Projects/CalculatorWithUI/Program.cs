1b95f238a2983b78ba6779879b386dcb2ca43aac
seDrakonkill <samuel@kjcpa.com>
2023-11-10 19:55:02 -0600

    Mdae a couple of changes to the general thing.
    Will be brushing up on Git tomorrow and relearning some important coding concepts for the git bash

--- Projects/CalculatorWithUI/Program.cs
+++ Projects/CalculatorWithUI/Program.cs
@@ -25,22 +25,22 @@ namespace CalculatorWithUI
         }
         public static float Subtract(float var1, float var2)
         {
-            storedVariable = var1 + var2;
+            storedVariable = var1 - var2;
             return 0;
         }
         public static float Multiply(float var1, float var2)
         {
-            storedVariable = var1 + var2;
+            storedVariable = var1 * var2;
             return 0;
         }
         public static float Divide(float var1, float var2)
         {
-            storedVariable = var1 + var2;
+            storedVariable = var1 / var2;
             return 0;
         }
         public static float SqRoot(float var1, float var2)
         {
-            storedVariable = var1 + var2;
+            storedVariable = var1;
             return 0;
         }
         public static float Log(float var1, float var2)
